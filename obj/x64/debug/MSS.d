9f1f25ba8835798c4046d824d5f044603aa8a735
Dyavil <dyavil.q@gmail.com>
2017-03-29 20:07:25 +0200

    update ignore

---
+++ obj/x64/debug/MSS.d
@@ -0,0 +1,27 @@
+obj/x64/debug/MSS.o: src/master_MecaSim/src-etudiant/MSS.cpp \
+ src/gKit/vec.h src/master_MecaSim/src-etudiant/MSS.h \
+ src/master_MecaSim/src-etudiant/Noeuds.h src/gKit/draw.h src/gKit/mesh.h \
+ src/gKit/glcore.h src/gKit/vec.h src/gKit/mat.h src/gKit/color.h \
+ src/gKit/orbiter.h src/gKit/window.h
+
+src/gKit/vec.h:
+
+src/master_MecaSim/src-etudiant/MSS.h:
+
+src/master_MecaSim/src-etudiant/Noeuds.h:
+
+src/gKit/draw.h:
+
+src/gKit/mesh.h:
+
+src/gKit/glcore.h:
+
+src/gKit/vec.h:
+
+src/gKit/mat.h:
+
+src/gKit/color.h:
+
+src/gKit/orbiter.h:
+
+src/gKit/window.h:
---
+++ obj/x64/debug/SolveurImpl.d
@@ -0,0 +1,77 @@
+obj/x64/debug/SolveurImpl.o: \
+ src/master_MecaSim/src-etudiant/SolveurImpl.cpp src/gKit/vec.h \
+ src/master_MecaSim/src-etudiant/ObjetSimule.h src/gKit/mesh.h \
+ src/gKit/glcore.h src/gKit/vec.h src/gKit/mat.h src/gKit/color.h \
+ src/master_MecaSim/src-etudiant/MSS.h \
+ src/master_MecaSim/src-etudiant/Noeuds.h src/gKit/draw.h src/gKit/mesh.h \
+ src/gKit/orbiter.h src/gKit/window.h \
+ src/master_MecaSim/src-etudiant/Properties.h \
+ src/master_MecaSim/src-etudiant/Viewer.h src/gKit/glcore.h \
+ src/gKit/window.h src/gKit/program.h src/gKit/buffer.h \
+ src/gKit/texture.h src/gKit/image.h src/gKit/image_io.h src/gKit/mat.h \
+ src/gKit/orbiter.h src/gKit/app.h \
+ src/master_MecaSim/src-etudiant/Scene.h \
+ src/master_MecaSim/src-etudiant/ObjetSimuleParticule.h \
+ src/master_MecaSim/src-etudiant/SolveurExpl.h \
+ src/master_MecaSim/src-etudiant/ObjetSimuleMSS.h \
+ src/master_MecaSim/src-etudiant/SolveurImpl.h
+
+src/gKit/vec.h:
+
+src/master_MecaSim/src-etudiant/ObjetSimule.h:
+
+src/gKit/mesh.h:
+
+src/gKit/glcore.h:
+
+src/gKit/vec.h:
+
+src/gKit/mat.h:
+
+src/gKit/color.h:
+
+src/master_MecaSim/src-etudiant/MSS.h:
+
+src/master_MecaSim/src-etudiant/Noeuds.h:
+
+src/gKit/draw.h:
+
+src/gKit/mesh.h:
+
+src/gKit/orbiter.h:
+
+src/gKit/window.h:
+
+src/master_MecaSim/src-etudiant/Properties.h:
+
+src/master_MecaSim/src-etudiant/Viewer.h:
+
+src/gKit/glcore.h:
+
+src/gKit/window.h:
+
+src/gKit/program.h:
+
+src/gKit/buffer.h:
+
+src/gKit/texture.h:
+
+src/gKit/image.h:
+
+src/gKit/image_io.h:
+
+src/gKit/mat.h:
+
+src/gKit/orbiter.h:
+
+src/gKit/app.h:
+
+src/master_MecaSim/src-etudiant/Scene.h:
+
+src/master_MecaSim/src-etudiant/ObjetSimuleParticule.h:
+
+src/master_MecaSim/src-etudiant/SolveurExpl.h:
+
+src/master_MecaSim/src-etudiant/ObjetSimuleMSS.h:
+
+src/master_MecaSim/src-etudiant/SolveurImpl.h:
